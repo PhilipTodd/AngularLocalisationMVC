dde82c97ea5b911fc9e4642b0e1dafe2562e572a
PhilipTodd <philip.todd@gmail.com>
2016-10-24 12:46:12 +1100

    Add project files.

---
+++ AngularLocalisationMVC/Scripts/Vendor/rb-select-all-on-focus.js
@@ -0,0 +1,20 @@
+﻿angular.module('rb.select-all-on-focus', [])
+  .directive('rbSelectAllOnFocus', function () {
+      'use strict';
+
+      return {
+          restrict: 'A',
+          scope: {
+              rbSelectAllOnFocus: '=',
+          },
+          link: function (scope, element, attrs, ctrl) {
+              element.on('focus', function () {
+                  var domElement = element[0];
+                  if (scope.rbSelectAllOnFocus &&
+                      angular.isFunction(domElement.setSelectionRange)) {
+                      domElement.setSelectionRange(0, domElement.value.length);
+                  }
+              });
+          },
+      };
+  });
